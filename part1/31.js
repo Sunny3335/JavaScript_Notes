04a2b0625ab610375a1eb3e7fcc9f5ea320732b2
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-14 02:43:55 +0530

    changes saved

--- part1/31.js
+++ part1/31.js
@@ -8,3 +8,12 @@
 // fruits=["cvrev","rvese"];// we can not do this because you are trying to make a new array at the same location.
 // console.log(fruits);
 
+const arr1 = ['banana', 'apple', 'peach', 'mango'];
+// arr1 = ['apple', 'peach']; // can not do because we are not allow to make new array with same arrayName , instead of that we should create a new variable but we can update the array by using push pull operator.
+
+const arr2 = ['banana', 'apple', 'peach', 'mango'];
+arr2.unshift("jch"); // add in front.
+console.log(arr1);
+console.log(arr2);
+console.log(&arr1);
+
--- part1/35.js
+++ part1/35.js
@@ -6,18 +6,15 @@ const myArray = ["value1"];
 // console.log("value of myvar1", myvar1);
 // console.log("value of myvar2", myvar2);
 
-
 let [myvar1, myvar2, ...myNewArray] = myArray;
-// let myNewArray=myArray.slice(2); // we can also do by destructuring in the above code using the ...  operator.
-// myvar1="sunny";
+// let myNewArray=myArray.slice(2); // we can also do by destructuring in the above code using the  ...operator.
+// myvar1="sunny"; // in destructuring we should use let in place of const because  it is mutable.
 // here we use let not const because if we use const then we can not able to change the value of variable.
 // if there is less element in the array than the variable defined then it will defined the undefined value.
 // to avoid undefined we use comma to separate or escape the variable.
 // let[myvar1, ,myvar2]=myArray;
 console.log("value of myvar1", myvar1);
-console.log("value of myvar2", myvar2); // it gives undefined in case of not shortage of array element relative to new defined variable.
+console.log("value of myvar2", myvar2); // it gives undefined in case of shortage of array element relative to new defined variable.
 console.log(myNewArray);
 
 
-// my practice
-let[obj1,obj2,obj3]=myArray;
